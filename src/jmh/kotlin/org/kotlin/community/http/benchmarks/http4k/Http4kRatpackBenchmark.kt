91d739ac3268d763ad6d77897b0fe65ad8d66e54
Javier Salinas <jsalinaspolo@gmail.com>
2023-07-25 10:01:45 +0200

    Fix kf

--- src/jmh/kotlin/org/kotlin/community/http/benchmarks/http4k/Http4kRatpackBenchmark.kt
+++ src/jmh/kotlin/org/kotlin/community/http/benchmarks/http4k/Http4kRatpackBenchmark.kt
@@ -4,7 +4,6 @@ import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.server.Http4kServer
-import org.http4k.server.Netty
 import org.http4k.server.Ratpack
 import org.http4k.server.asServer
 import org.kotlin.community.http.benchmarks.HttpBenchmarkBase
